4a4557542c99baac8c6353c911ade0e8b43dad4e
thatjiaozi <jj.lopezjaimez@gmail.com>
2023-10-24 19:35:28 -0400

    Cleanup unused features of buzzer

--- main.go
+++ main.go
@@ -33,11 +33,6 @@ var (
 	sourceFilesPath    = flag.String("src_path", "/root/sourceFiles", "The fuzzer will look for source files to visualize the coverage at this path")
 	metricsServerAddr  = flag.String("metrics_server_addr", "0.0.0.0", "Address that the metrics server will listen to at")
 	metricsServerPort  = flag.Uint("metrics_server_port", 8080, "Port that the metrics server will listen to at")
-	verbosity          = flag.Int("verbosity_level", 0, "How verbose the binary behaves. Possible values are: 0, 1, 2. Where 0 is quiet mode and 2 is the most verbose mode (default = 0)")
-	localIP            = flag.String("local_ip", "", "Address to which bind the Registry/Execution server depending on whether the binary runs in server or client mode")
-	localPort          = flag.Uint("local_port", 4444, "Port at which the local Registry/Execution server will listen at (default = 4444)")
-	remoteIP           = flag.String("remote_ip", "", "If run in client mode, this ip will be used to contact the server")
-	remotePort         = flag.Uint("remote_port", 4444, "Which port is the server running in case of client mode, or which port should the server run in case of server mode (default = 4444)")
 )
 
 func main() {
@@ -46,8 +41,7 @@ func main() {
 	metricsUnit := metrics.New(*metricsThreshold, *coverageBufferSize, *vmLinuxPath, *sourceFilesPath, *metricsServerAddr, uint16(*metricsServerPort))
 	if err := controlUnit.Init(&units.Executor{
 		MetricsUnit: metricsUnit,
-	}, *runMode, *fuzzStrat,
-		*verbosity, *localIP, *localPort, *remoteIP, *remotePort); err != nil {
+	}, *runMode, *fuzzStrat); err != nil {
 		log.Fatalf("failed to init control unit: %v", err)
 	}
 
