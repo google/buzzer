f7baf5f021c1512edd7281615be634ad0781d14f
thatjiaozi <jj.lopezjaimez@gmail.com>
2023-10-24 19:41:44 -0400

    fix unit tests

--- pkg/units/control_unit_test.go
+++ pkg/units/control_unit_test.go
@@ -15,7 +15,6 @@
 package units
 
 import (
-	"errors"
 	"testing"
 )
 
@@ -24,51 +23,19 @@ func TestControlUnitInitSuccess(t *testing.T) {
 		testName   string
 		runMode    string
 		fuzzStrat  string
-		verbosity  int
-		localIP    string
-		localPort  uint
-		remoteIP   string
-		remotePort uint
-
-		eVerbosity VerbosityLevel
-		eRunMode   RunMode
 	}{
 		{
 			testName:   "Server mode info verbosity",
 			runMode:    "server",
 			fuzzStrat:  "parse_verifier_log",
-			verbosity:  1,
-			localIP:    "127.0.0.1",
-			localPort:  4444,
-			remoteIP:   "127.0.0.1",
-			remotePort: 4444,
-
-			eVerbosity: Info,
-			eRunMode:   Server,
 		}, {
 			testName:   "Client mode quiet verbosity",
 			runMode:    "client",
 			fuzzStrat:  "parse_verifier_log",
-			verbosity:  0,
-			localIP:    "127.0.0.1",
-			localPort:  4444,
-			remoteIP:   "127.0.0.1",
-			remotePort: 4444,
-
-			eVerbosity: Quiet,
-			eRunMode:   Client,
 		}, {
 			testName:   "Standalone mode Verbose verbosity",
 			runMode:    "standalone",
 			fuzzStrat:  "parse_verifier_log",
-			verbosity:  2,
-			localIP:    "127.0.0.1",
-			localPort:  4444,
-			remoteIP:   "127.0.0.1",
-			remotePort: 4444,
-
-			eVerbosity: Verbose,
-			eRunMode:   Standalone,
 		},
 	}
 
@@ -76,73 +43,13 @@ func TestControlUnitInitSuccess(t *testing.T) {
 		t.Run(tc.testName, func(t *testing.T) {
 			t.Logf("Running test case %s", tc.testName)
 			cu := &ControlUnit{}
-			if err := cu.Init(&Executor{}, tc.runMode, tc.fuzzStrat, tc.verbosity, tc.localIP, tc.localPort, tc.remoteIP, tc.remotePort); err != nil {
+			if err := cu.Init(&Executor{}, tc.runMode, tc.fuzzStrat); err != nil {
 				t.Fatalf("Unexpected error %s", err)
-			}
-
-			if cu.GetVerbosityLevel() != tc.eVerbosity {
-				t.Fatalf("cu.GetVerbosityLevel() = %d, want verbosity %d ", cu.vl, tc.eVerbosity)
-			}
-
-			if cu.GetRunMode() != tc.eRunMode {
-				t.Fatalf("cu.GetRunMode() = %s, want run mode %s ", cu.rm, tc.eRunMode)
 			}
 
 			if !cu.IsReady() {
 				t.Fatalf("Control unit was not marked as ready at the end of init")
 			}
 		})
-	}
-}
-
-func TestControlUnitInitFailure(t *testing.T) {
-	tests := []struct {
-		testName   string
-		runMode    string
-		fuzzStrat  string
-		verbosity  int
-		localIP    string
-		localPort  uint
-		remoteIP   string
-		remotePort uint
-
-		eError error
-	}{
-		{
-			testName:   "Error due to unknown run Mode",
-			runMode:    "unknown",
-			fuzzStrat:  "parse_verifier_log",
-			verbosity:  1,
-			localIP:    "127.0.0.1",
-			localPort:  4444,
-			remoteIP:   "127.0.0.1",
-			remotePort: 4444,
-
-			eError: errors.New("unknown run mode: unknown"),
-		}, {
-			testName:   "Error due to unknown verbosity level",
-			runMode:    "server",
-			fuzzStrat:  "parse_verifier_log",
-			verbosity:  -1,
-			localIP:    "127.0.0.1",
-			localPort:  4444,
-			remoteIP:   "127.0.0.1",
-			remotePort: 4444,
-
-			eError: errors.New("unknown verbosity level: -1"),
-		},
-	}
-
-	for _, tc := range tests {
-		t.Logf("Running test case %s", tc.testName)
-		cu := &ControlUnit{}
-		err := cu.Init(&Executor{}, tc.runMode, tc.fuzzStrat, tc.verbosity, tc.localIP, tc.localPort, tc.remoteIP, tc.remotePort)
-		if err == nil {
-			t.Fatalf("Control Unit Init Succeeded unexpectedly")
-		}
-
-		if err.Error() != tc.eError.Error() {
-			t.Fatalf("Got error %s, but expected %s", err, tc.eError)
-		}
 	}
 }
